3ba4c5eab3718ec9f921921b9d462e8ebfd9449b
llamalimes <llamalimes@outlook.com>
2021-04-08 20:31:40 -0400

    update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+sys.path.append('/home/bitnami/SolarPV')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/SolarPV/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
